1f1dc9b3a2eba68ea7b8709c91ca7cb05d9f8d0c
Brenek Harrison <brenekharrison@gmail.com>
2023-11-15 23:46:03 -0700

    Cleanup javascript and use modulus for countdown instead of subtracting a bunch

--- static/index.js
+++ static/index.js
@@ -1,27 +1,35 @@
-"use strict";
-var targetDate = new Date("2024-12-01T00:00:00-0700");
-var MS_IN_DAY = 1000 * 60 * 60 * 24;
-var MS_IN_HOUR = 1000 * 60 * 60;
-var MS_IN_MIN = 1000 * 60;
-var MS_IN_SEC = 1000;
+const targetDate = new Date("2024-12-01T00:00:00-0700");
+const MS_IN_DAY = 1000 * 60 * 60 * 24;
+const MS_IN_HOUR = 1000 * 60 * 60;
+const MS_IN_MIN = 1000 * 60;
+const MS_IN_SEC = 1000;
+
 function main() {
     updateTime();
     setInterval(updateTime, 1000);
 }
+
 function updateTime() {
-    var currentTime = new Date();
-    var timeLeft = targetDate.getTime() - currentTime.getTime();
-    var days = Math.floor(timeLeft / MS_IN_DAY);
-    var hours = Math.floor((timeLeft - (days * MS_IN_DAY)) / MS_IN_HOUR);
-    var minutes = Math.floor((timeLeft - (days * MS_IN_DAY + hours * MS_IN_HOUR)) / MS_IN_MIN);
-    var seconds = Math.floor((timeLeft - (days * MS_IN_DAY + hours * MS_IN_HOUR + minutes * MS_IN_MIN)) / MS_IN_SEC);
-    var elem = document.getElementById("timer");
+    const currentTime = new Date();
+    const timeLeft = targetDate.getTime() - currentTime.getTime();
+
+    const days = Math.floor(timeLeft / MS_IN_DAY);
+    const hours = Math.floor((timeLeft % MS_IN_DAY) / MS_IN_HOUR);
+    const minutes = Math.floor((timeLeft % MS_IN_HOUR) / MS_IN_MIN);
+    const seconds = Math.floor((timeLeft % MS_IN_MIN) / MS_IN_SEC);
+
+    const elem = document.getElementById("timer");
     if (elem === null) {
         return;
     }
-    var hourStr = hours.toLocaleString(undefined, { minimumIntegerDigits: 2, useGrouping: false });
-    var minStr = minutes.toLocaleString(undefined, { minimumIntegerDigits: 2, useGrouping: false });
-    var secStr = seconds.toLocaleString(undefined, { minimumIntegerDigits: 2, useGrouping: false });
+
+    const hourStr = hours.toLocaleString(undefined, { minimumIntegerDigits: 2, useGrouping: false });
+    const minStr = minutes.toLocaleString(undefined, { minimumIntegerDigits: 2, useGrouping: false });
+    const secStr = seconds.toLocaleString(undefined, { minimumIntegerDigits: 2, useGrouping: false });
+
     elem.innerText = "".concat(days, " Days ").concat(hourStr, ":").concat(minStr, ":").concat(secStr);
+
+    if (timeLeft < 0) {
+        elem.innerText = "Season 2 is released!";
+    }
 }
-//# sourceMappingURL=index.js.map
